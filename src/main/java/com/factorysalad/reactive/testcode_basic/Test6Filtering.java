dc68182c6c804efd298fed02a14b073682da804e
arter365 <arter@factorysalad.com>
2021-05-17 18:00:59 +0900

    5일차 학습

--- src/main/java/com/factorysalad/reactive/testcode_basic/Test6Filtering.java
+++ src/main/java/com/factorysalad/reactive/testcode_basic/Test6Filtering.java
@@ -16,7 +16,10 @@ public class Test6Filtering {
     public static void main(String[] args) {
         Test6Filtering t6 = new Test6Filtering();
         t6.filter();
+        t6.numberDistinct();
     }
+
+
 
     public void filter() {
         System.out.println("filter start -----");
@@ -29,6 +32,27 @@ public class Test6Filtering {
                 .filter(p -> p.getAge() > 20)
                 .subscribe(p -> log.info(p.toString()));
         System.out.println("filter end -----");
+    }
+
+    public void numberDistinct() {
+        System.out.println("numberFilter start -----");
+        Flux.fromIterable(List.of(1,1,2,2))
+                .distinct()
+                .subscribe(p -> log.info(p.toString()));
+        System.out.println("numberFilter end -----");
+    }
+
+    public void stringDistinct() {
+        System.out.println("distinct start -----");
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person(1, "강감찬", 19));
+        personList.add(new Person(1, "홍길동", 23));   // id를 1로 수정
+        personList.add(new Person(3, "이순신", 30));
+
+        Flux.fromIterable(personList)
+                .distinct()
+                .subscribe(p -> log.info(p.toString()));
+        System.out.println("distinct end -----");
     }
 
 }
