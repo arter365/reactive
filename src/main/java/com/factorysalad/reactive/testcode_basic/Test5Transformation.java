67a4236dfe9b9a0989b6017183c8a181440275f4
arter365 <arter@factorysalad.com>
2021-07-02 10:43:24 +0900

    11일차 학습

--- src/main/java/com/factorysalad/reactive/testcode_basic/Test5Transformation.java
+++ src/main/java/com/factorysalad/reactive/testcode_basic/Test5Transformation.java
@@ -41,10 +41,10 @@ public class Test5Transformation {
 
     /*
     Flux나 Mono로 asynchronous call을 하고 싶다.
-    이럴때는 flatMap을 사용한다. flatMap은 변경하는 Function을 인자로 받아서 Publisher<U>를 리턴한다. (Publisher == Flat or Mono)
+    이럴때는 flatMap을 사용한다. flatMap은 변경하는 Function을 인자(람다방식을 생각해야 한다)로 받아서 Publisher<X>를 리턴한다. (Publisher == Flat or Mono)
     비동기적으로 변경하는 내용을 Publisher로 감싸고 있다.
-    map을 사용하면 Flux<Publisher<U>>를 리턴하여 사용이 어렵다.
-    반면에 FlatMap을 사용하면
+    하지만 map과 FlatMap의 큰 차이는 동기와 비동기의 차이도 있지만 map을 사용하면 Flux<Publisher<U>>를 리턴하여 사용이 어렵고
+    FlatMap을 사용하면 이전 Publisher의 Flux를 처리하여 한개 이상의 Flux를 리턴한다. 또한 여러 Flux를 한줄로 만들어서 리턴해 준다.
     */
     public void flatMap() {
         System.out.println("flatMap start -----");
@@ -56,7 +56,8 @@ public class Test5Transformation {
         Flux.fromIterable(personList)
                 .flatMap(p -> {
                     p.setAge(p.getAge() + 10);
-                    return Mono.just(p);
+                    // Mono 또는 Just를 만들어서 리턴한다. flatMap은 리턴한다고 정의되어 있을 뿐 구현은 람다식을 사용하여 직접해야 한다.
+                    return Flux.just(p);
                 })
                 .subscribe(p -> log.info(p.toString()));
         System.out.println("flatMap end -----");
